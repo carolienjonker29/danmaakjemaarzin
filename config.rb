d0d8a91fb7a381d6a48fa13f250b48f0a9b2347b
Carolien Jonker <carolien.jonker@gmail.com>
2016-10-16 22:54:23 +0200

    adding a navbar, adding bootstrap, filling up the page, linking menu items

--- config.rb
+++ config.rb
@@ -69,4 +69,26 @@ configure :build do
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+  # # Use relative URLs
+  activate :relative_assets
+  set :relative_links, true
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+  #
+  require_relative "./lib/build_cleaner"
+  activate :build_cleaner
 end
+
+# Deployment
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true
+
+  # Optional Settings
+  # deploy.remote = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
